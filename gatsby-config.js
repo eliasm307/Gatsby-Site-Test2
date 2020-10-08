d1a466de90b10b1e44206501f9ff00aa28f41fb2
eliasm307 <eliaschakomangoro@gmail.com>
2020-10-08 15:10:05 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,89 @@
+const path = require("path");
+const { title, keywords, description, author, defaultLang, trackingId } = require("./config/site");
+
+module.exports = {
+  siteMetadata: {
+    title,
+    keywords,
+    description,
+    author,
+  },
+  plugins: [
+    /*{
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId,
+      },
+    },*/
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "markdown",
+        path: `${__dirname}/content`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/content/assets/images`,
+      },
+    },
+    /*{
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: title,
+        short_name: "Agency",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#fed136",
+        display: "minimal-ui",
+        icon: "content/assets/gatsby-icon.png",
+      },
+    },*/
+    "gatsby-plugin-eslint",
+    "gatsby-plugin-react-helmet",
+
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        data: `@import "core.scss";`,
+        includePaths: [path.resolve(__dirname, "src/style")],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-prefetch-google-fonts",
+      options: {
+        fonts: [
+          {
+            family: "Montserrat",
+            variants: [400, 700],
+          },
+          {
+            family: "Kaushan+Script",
+          },
+          {
+            family: "Droid+Serif",
+            variants: [400, 700, "400italic", "700italic"],
+          },
+          {
+            family: "Roboto+Slab",
+            variants: [400, 100, 300, 700],
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-i18n",
+      options: {
+        langKeyDefault: defaultLang,
+        useLangKeyLayout: false,
+        pagesPaths: ["/content/"],
+      },
+    },
+  ],
+};
